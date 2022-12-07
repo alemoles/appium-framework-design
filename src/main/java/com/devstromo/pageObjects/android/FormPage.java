bad19bc1a3b73fe730a82816e4b18fd926806aa0
alemoles <alemolerio@gmail.com>
2022-12-07 20:55:10 -0300

    Inherit Android Actions class to page object classes for reusable methods

--- src/main/java/com/devstromo/pageObjects/android/FormPage.java
+++ src/main/java/com/devstromo/pageObjects/android/FormPage.java
@@ -4,15 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import com.devstromo.utils.AndroidActions;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class FormPage {
+public class FormPage extends AndroidActions {
 
     private final AndroidDriver driver;
 
     public FormPage(AndroidDriver driver) {
+        super(driver);
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
@@ -27,6 +30,9 @@ public class FormPage {
 
     @AndroidFindBy(xpath = "//android.widget.RadioButton[@text='Male']")
     private WebElement maleOption;
+
+    @AndroidFindBy(id = "android:id/text1")
+    private WebElement countrySelection;
 
     @AndroidFindBy(xpath = "//android.widget.TextView[@text='Argentina']")
     private WebElement country;
