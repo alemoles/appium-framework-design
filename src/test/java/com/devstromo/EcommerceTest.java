ca10e3eca3fdbcb32e8ed9771f97be7e35e9d432
alemoles <alemolerio@gmail.com>
2022-12-13 21:27:20 -0300

    Implement Test Strategy on deciding how to run tests in a file

--- src/test/java/com/devstromo/EcommerceTest.java
+++ src/test/java/com/devstromo/EcommerceTest.java
@@ -214,42 +214,7 @@ public class EcommerceTest extends BaseTest {
         cartPage.submitOrder();
     }
 
-    @Test
-    public void fillFormErrorValidationTCTest() throws InterruptedException {
 
-        driver.hideKeyboard();
-        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
-            .click();
-        driver.findElement(AppiumBy.id("android:id/text1"))
-            .click();
-        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
-            .click();
-        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
-            .click();
-        String toastErrorMessage = driver.findElement(AppiumBy.xpath("(//android.widget.Toast)[1]"))
-            .getAttribute("name");
-        assertEquals("Please enter your name", toastErrorMessage);
-    }
-
-    @Test
-    public void fillFormPositiveFlowTest() throws InterruptedException {
-        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
-            .sendKeys("Ale M");
-        driver.hideKeyboard();
-        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
-            .click();
-        driver.findElement(AppiumBy.id("android:id/text1"))
-            .click();
-        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
-            .click();
-        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
-            .click();
-        int size = driver.findElements(AppiumBy.xpath("(//android.widget.Toast)[1]"))
-            .size();
-        assertEquals(0, size);
-    }
 
     private ProductCatalog startPage() {
         formPage.setNameField("Ale M");
